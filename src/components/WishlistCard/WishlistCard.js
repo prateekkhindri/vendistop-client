8b0036c144dce4cf6f205ab066807131ba1f9b66
PrateekKhindri <prateek.khindri90@gmail.com>
2023-07-30 17:30:39 +1000

    T3A2-B-026-Added WishlistCard component and all relevant API endpoints [Prateek]

---
+++ src/components/WishlistCard/WishlistCard.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+import { removeProductFromWishlist } from "../../pages/Wishlist/wishlistAction.js";
+import { useDispatch } from "react-redux";
+
+export const WishlistCard = ({ _id, image, title, price }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="mx-3 my-4">
+      <div className="grid grid-cols-3 border border-[#EBEBEB] rounded-lg p-3 bg-[#F8F8F8]">
+        {/* Image title and price section */}
+        <div className="flex items-start justify-start col-span-2 sm:col-span-1 sm:items-center">
+          <div className="flex items-center max-w-[70px] gap-x-4">
+            <Link to={`/product/${_id}`}>
+              <img
+                className="object-cover w-full h-full rounded-lg aspect-square"
+                src={image}
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center h-full pl-3 sm:items-center">
+            <h3 className="text-xs lg:text-sm text-[#464545] font-medium line-clamp-1">
+              {title}
+            </h3>
+            <p className="pt-1 text-xs sm:hidden">{`$${price}`}</p>
+          </div>
+        </div>
+        {/* Price for desktop view */}
+        <div className="justify-center hidden h-full pl-3 sm:flex sm:items-center">
+          <p className="pt-1 text-xs lg:text-sm">{`$${price}`}</p>
+        </div>
+        {/* Add to cart and remove from favorite buttons*/}
+        <div className="flex items-center justify-end pt-3">
+          <div className="p-1 bg-white rounded-md cursor-pointer">
+            <Icon
+              icon="uil:shopping-cart"
+              className="hover:text-red-400 lg:text-xl text-[#696969]"
+            />
+          </div>
+          <div
+            onClick={() => dispatch(removeProductFromWishlist(_id))}
+            className="p-1 ml-2 bg-white rounded-md cursor-pointer"
+          >
+            <Icon
+              icon="material-symbols:delete"
+              className="hover:text-red-400 lg:text-xl text-[#696969]"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Footer, WishlistCard } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWishlist } from "./wishlistAction";
 
 const Wishlist = () => {
-  return <div>Wishlist</div>;
+  const dispatch = useDispatch();
+
+  const { wishlist } = useSelector((state) => state.wishlistStore);
+
+  useEffect(() => {
+    !wishlist.length && dispatch(fetchWishlist());
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="bg-[#FDF9FF] min-h-[calc(100vh-70px)] px-3">
+        <section className="max-w-screen-xl mx-auto">
+          <div className="px-2 py-8">
+            <h1 className="text-[#2c3c3c] text-xl font-semibold">Wishlist</h1>
+          </div>
+          <div className="pb-40">
+            <div className="max-w-[930px] mx-auto bg-white rounded-xl p-4 lg:p-8">
+              {wishlist.length === 0 ? (
+                <div className="flex justify-center items-center h-64">
+                  <label className="text-lg md:text-2xl text-[#ADADAD]">
+                    Wishlist is empty
+                  </label>
+                </div>
+              ) : (
+                <>
+                  {wishlist.map((item) => {
+                    return (
+                      <WishlistCard
+                        key={item._id}
+                        _id={item._id}
+                        image={item.image}
+                        title={item.name}
+                        price={item.price}
+                      />
+                    );
+                  })}
+                </>
+              )}
+            </div>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Wishlist;
