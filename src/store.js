87fdc31a5445dbc818a7cdc8608483ebe0702ac4
PrateekKhindri <prateek.khindri90@gmail.com>
2023-07-30 13:03:11 +1000

    T3A2-B-021-Added the orderReducer to the redux store [Prateek]

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./pages/login-register/loginRegisterSlice";
 import categoryReducer from "./pages/categories/categorySlice.js";
 import productReducer from "./pages/upload-product/productSlice.js";
+import orderReducer from "./pages/orders/orderSlice.js";
 
 const store = configureStore({
   reducer: {
     adminUser: loginReducer,
     categoriesStore: categoryReducer,
     productStore: productReducer,
+    orderStore: orderReducer,
   },
 });
 
