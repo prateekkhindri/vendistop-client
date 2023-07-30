28230e019bc57f9e07eb94e1db32d87527067875
NatalieTucknott <13875@coderacademy.edu.au>
2023-07-30 13:25:25 +1000

    T3A2-B-022-Added admin Dashboard page [Nat]

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,13 @@ import Login from "./login-register/Login";
 import Registration from "./login-register/Registration";
 import PasswordResetEmail from "./PasswordResetEmail/PasswordResetEmail";
 import DashboardCategories from "./categories/DashboardCategories";
+import Dashboard from "./Dashboard/Dashboard";
 
 // Export all pages
-export { Login, Registration, PasswordResetEmail, DashboardCategories };
+export {
+  Login,
+  Registration,
+  PasswordResetEmail,
+  DashboardCategories,
+  Dashboard,
+};
