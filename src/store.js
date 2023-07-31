77beed7251199a36f42fcab6058204376beb443d
PrateekKhindri <prateek.khindri90@gmail.com>
2023-07-31 15:44:29 +1000

    T3A2-B-026,027-Modified store.js [Prateek]

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@ import loginReducer from "./pages/login-register/loginRegisterSlice";
 import categoryReducer from "./pages/categories/categorySlice.js";
 import productReducer from "./pages/upload-product/productSlice.js";
 import orderReducer from "./pages/orders/orderSlice.js";
+import wishlistReducer from "./pages/Wishlist/wishlistSlice.js";
+import cartReducer from "./pages/Cart/cartSlice.js";
 
 const store = configureStore({
   reducer: {
@@ -10,6 +12,8 @@ const store = configureStore({
     categoriesStore: categoryReducer,
     productStore: productReducer,
     orderStore: orderReducer,
+    wishlistStore: wishlistReducer,
+    cartStore: cartReducer,
   },
 });
 
